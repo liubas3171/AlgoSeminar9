046498e366df6ac218ee6e08da7a3f95a29a0ff5
liubas3171 <54552855+liubas3171@users.noreply.github.com>
2020-11-29 22:12:11 +0200

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,7 +1,10 @@
 def best_revenue(n):
+    if max_revs[n] != 0:
+        return max_revs[n]
     if n < 0:
         return 0
     if n == 0:
+        max_revs[n] = max(l[n], h[n])
         return max(l[n], h[n])
     l_res = l[n] + best_revenue(n - 1)
     h_res = h[n] + best_revenue(n - 2)
@@ -10,13 +13,16 @@ def best_revenue(n):
         res[n] = h[n]
     else:
         res[n] = l[n]
+    max_revs[n] = max(l_res, h_res)
     return max(l_res, h_res)
 
 
 if __name__ == "__main__":
     l = [10, 1, 10, 10]
     h = [5, 50, 5, 1]
+    # list for saving results (revenues)
+    max_revs = [0] * len(l)
+    # list for saving plan
     res = [0] * len(l)
-    # [0, 50, 10, 10]
     print(best_revenue(3))
     print(res)
